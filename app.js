9e6ded03ce3ece5a5c5d687f04a104abe9f670a2
Youcef <youcefcopy@gmail.com>
2025-12-01 16:49:52 +0100

    calling displaySlide() | adding a css class of display block

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // creating a node list
 const slides = document.querySelectorAll('.slides img');
 let slideIndex = 0;
-let intervalId = null;
+let intervalId = null; 
+
+initializeSlider();
 
 function initializeSlider() {
-    
+    slides[slideIndex].classList.add(displaySlide)
 }
 
 function showSlide(index) {
