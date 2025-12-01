76bf90c8b9a97c05a3a4f1b2ff62f3e370e45fca
Youcef <youcefcopy@gmail.com>
2025-12-01 16:54:27 +0100

    ! wait for all dom content to load

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 // creating a node list
-const slides = document.querySelectorAll('.slides img');
+const slides = document.querySelectorAll(".slides img");
 let slideIndex = 0;
-let intervalId = null; 
+let intervalId = null;
 
-initializeSlider();
+// ! wait for all dom content to load
+document.addEventListener("DOMContentLoaded", initializeSlider);
 
 function initializeSlider() {
-    slides[slideIndex].classList.add(displaySlide)
+  slides[slideIndex].classList.add("displaySlide");
 }
 
-function showSlide(index) {
-    
-}
+function showSlide(index) {}
 
-function prevSlide() {
-    
-}
+function prevSlide() {}
 
-function nextSlide() {
-    
-}
+function nextSlide() {}
