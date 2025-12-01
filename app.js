9694218e1af930161a3c296d22cded0ed20c55f7
Youcef <youcefcopy@gmail.com>
2025-12-01 16:47:00 +0100

    intervalID for setInterval() | setting up functions

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 // creating a node list
 const slides = document.querySelectorAll('.slides img');
 let slideIndex = 0;
+let intervalId = null;
 
+function initializeSlider() {
+    
+}
 
-
-
-
-
-
-
-
-
-
+function showSlide(index) {
+    
+}
 
 function prevSlide() {
     
