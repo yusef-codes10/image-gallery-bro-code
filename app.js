04633c64fdfb2702871707de150a960c931229e9
Youcef <youcefcopy@gmail.com>
2025-12-02 19:05:42 +0100

    necessary functions

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ const slides = document.querySelectorAll('.slides imgs');
 
 // ! 2- declaring the index to move through the node list
 let slideIndex = 0;
+
+
+// ! 3- necessary functions
+function initializeSlide() {
+
+}
+
+function showSlide(index) {
+  
+}
+
+function nextSlide() {
+  
+}
+
+function prevSlide() {
+  
+}
