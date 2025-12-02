dd5ec7e714326c7889a1f92098512db6068784a9
Youcef <youcefcopy@gmail.com>
2025-12-02 19:02:00 +0100

    declaring the node list of images

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-// creating a node list
-const slides = document.querySelectorAll(".slides img");
-let slideIndex = 0;
-let intervalId = null;
-
-// ! wait for all dom content to load
-document.addEventListener("DOMContentLoaded", initializeSlider);
-
-function initializeSlider() {
-  slides[slideIndex].classList.add("displaySlide");
-  intervalId =  setInterval(nextSlide, 5000); // call the next slide after 5s
-  console.log(intervalId);
-}
-
-function showSlide(index) {}
-
-function prevSlide() {}
-
-function nextSlide() {
-    slideIndex++;
-    showSlide(slideIndex);
-}
+// ! 1- the node list of images
+const slides = document.querySelectorAll('.slides imgs');
+console.log(slides);
