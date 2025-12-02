4e51dec37b460f085680151130dee197758fce44
Youcef <youcefcopy@gmail.com>
2025-12-02 19:25:32 +0100

    setting up the event listeners

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ console.log(slides);
 // ! 2- declaring the index to move through the node list
 let slideIndex = 0;
 
+const nextBtn = document.getElementById('nextBtn')
+const prevBtn = document.getElementById('prevBtn')
+
 // call the function after the dom content fully loaded
 document.addEventListener('DOMContentLoaded',
   initializeSlide
@@ -14,6 +17,9 @@ function initializeSlide() {
   // initilize the 1st img
   slides[slideIndex].classList.add('displaySlide');
 }
+
+// adding event listeners
+
 
 function showSlide(index) {
   // access the node list & remove the display class
